dbed5d23e68546a25d8a993c057d2d7d36002d68
noel319 <azeisl917@gmail.com>
2024-09-23 18:39:47 +0300

    resolve issue

--- delete_db.py
+++ delete_db.py
@@ -1,8 +1,8 @@
 import motor.motor_asyncio
 from pymongo.errors import ServerSelectionTimeoutError, OperationFailure
-import asyncio
+import asyncio,os
 from aiomultiprocess import Pool
-
+Search_FILE = 'search_files.txt'
 async def delete_databases_with_special_field_name(uri):
     # Create an async MongoDB client
     client = motor.motor_asyncio.AsyncIOMotorClient(uri)
@@ -14,6 +14,8 @@ async def delete_databases_with_special_field_name(uri):
 
         # Step 2: Iterate over each database
         for db_name in db_names:
+            if check_files (db_name):
+                continue
             db = client[db_name]
             
             # Step 3: Check if the 'main' collection exists in the database
@@ -36,18 +38,28 @@ async def delete_databases_with_special_field_name(uri):
                     print(f"Skipping database '{db_name}' due to error: {e}")
             else:
                 print(f"No 'main' collection found in database '{db_name}'.")
-    
+            with open(Search_FILE, 'a') as f:
+                f.write(f"{db_name}\n")
     except ServerSelectionTimeoutError:
         print("Unable to connect to MongoDB server. Check your connection or URI.")
     finally:
         # Close the connection asynchronously
-        await client.close()
+        client.close()
 
 async def start_delete(uri):
     # Create a pool for parallel execution
     async with Pool() as pool:
         await pool.apply(delete_databases_with_special_field_name, (uri,))
 
+def check_files(filename):
+    """Check if the given filename is in the migrated files list."""
+    if not os.path.exists(Search_FILE):
+        return False  
+
+    with open(Search_FILE, 'r') as f:
+        migrated_files = f.read().splitlines() 
+
+    return filename in migrated_files  
 # Example usage
 if __name__ == "__main__":
     uri = 'mongodb://twuser:moniThmaRtio@192.168.20.75:27017/admin'  # MongoDB URI
